3f142171811d7e83326d97b4b7606d453019804e
于豆豆 <13608676132@163.com>
2017-03-07 16:44:13 +0800

    Update SelectedBean.java

--- app/src/main/java/com/example/administrator/mycheckbox/SelectedBean.java
+++ app/src/main/java/com/example/administrator/mycheckbox/SelectedBean.java
@@ -1,11 +1,6 @@
 package com.example.administrator.mycheckbox;
 
-/**
- * 介绍：是否勾选中的Bean
- * 作者：zhangxutong
- * 邮箱：zhangxutong@imcoming.com
- * 时间： 2016/9/29.
- */
+
 
 public class SelectedBean {
     private boolean isSelected;
